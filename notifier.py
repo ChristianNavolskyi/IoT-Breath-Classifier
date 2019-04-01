7a66ab2573408a7d3f6fa213c4306810786a6a6a
Christian Navolskyi <c.navolskyi@gmail.com>
2019-04-01 13:44:05 +0200

    Added support for location sending and changed to post request.

--- notifier.py
+++ notifier.py
@@ -1,7 +1,37 @@
+import json
+import logging
+
 import requests
 
 
 class Notifier:
     def __init__(self):
-        self.notification_url = "http://localhost:8080/emergency"
-        requests.post(self.notification_url)
+        self.base_url = "http://localhost:8080/"
+        self.notification_url = self.base_url + "emergency/"
+
+        try:
+            requests.get(self.base_url)
+            self.working = True
+        except requests.exceptions.ConnectionError:
+            self.working = False
+            logging.warning("Telegram endpoint is not running!")
+
+    def send_emergency(self, message=None, send_location=False):
+        data = {}
+
+        if message:
+            data["message"] = str(message)
+
+        if send_location:
+            lat, lon = self.get_lon_lat()
+
+            data[lon] = lon
+            data[lat] = lat
+
+        requests.post(self.notification_url, data=data)
+
+    def get_lon_lat(self):
+        send_url = 'http://freegeoip.net/json'
+        r = requests.get(send_url)
+        j = json.loads(r.text)
+        return (j['latitude']), (j['longitude'])
