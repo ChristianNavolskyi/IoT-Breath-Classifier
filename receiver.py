37eaa320d5248e2be7642442fa6034dca96d4934
Christian Navolskyi <c.navolskyi@gmail.com>
2019-04-02 14:08:15 +0200

    Changed to use with simulation data.

--- receiver.py
+++ receiver.py
@@ -19,7 +19,7 @@ class Receiver(App):
         App.__init__(self)
 
         self.sample_length = 26
-        self.x_limit = 100
+        self.x_limit = 10
         self.x_value_range = range(self.x_limit)
         self.values = BoundedList(self.x_limit)
 
@@ -32,29 +32,28 @@ class Receiver(App):
         self.Bind(wx.EVT_TIMER, self.get_sample, self.timer)
         self.isLogging = False
 
-    def MainLoop(self):
-        self.visualiser.Show()
-        App.MainLoop(self)
+    def get_sample(self, value=None, event=None):
+        if self.ser.is_open:
+            sample_string = self.ser.readline()
+            self.visualiser.append_log(sample_string)
+            logging.debug("Receiving data: {0}".format(sample_string))
 
-    def get_sample(self, event=None):
-        sample_string = self.ser.readline()
-        self.visualiser.append_log(sample_string)
-        logging.debug("Receiving data: {0}".format(sample_string))
+            if len(sample_string) == self.sample_length:
+                sample_string = sample_string[0:-1]
+                sample_values = sample_string.split()
+                value = int(sample_values[0])
+        elif not value:
+            return
 
-        if len(sample_string) == self.sample_length:
-            sample_string = sample_string[0:-1]
-            sample_values = sample_string.split()
-            value = int(sample_values[0])
+        self.values.add_value(value)
+        self.value_logger.info("{0}".format(value))
 
-            self.values.add_value(value)
-            self.value_logger.info("{0}".format(value))
-
-            self.visualiser.update_plot([self.values],
-                                        1,
-                                        self.x_limit,
-                                        self.x_value_range,
-                                        y_lower_limit=self.values.max() - 10,
-                                        y_upper_limit=self.values.min() + 10)
+        self.visualiser.update_plot([self.values],
+                                    1,
+                                    self.x_limit,
+                                    self.x_value_range,
+                                    y_lower_limit=self.values.max() - 10,
+                                    y_upper_limit=self.values.min() + 10)
 
     def on_start_stop_button(self, event):
         if not self.isLogging:
